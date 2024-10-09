ac5a66c26491044eaa85d4a1934fc4051f6aef6e
joj <young2k15@tutanota.com>
2024-10-09 17:53:26 -0400

    autoscaling fix

--- lib/stacks/autoscaling-stack.ts
+++ lib/stacks/autoscaling-stack.ts
@@ -46,10 +46,12 @@ export class AutoScalingStack extends AwsStackBase {
                    metricName: "CPUReservation",
                    namespace: "AWS/ECS",
                    statistic: "Average", 
-                   metricDimension: [{
+                   metricDimension: [
+                   {
                     name: "ClusterName",
                     value: props.ecsClusterName,
-                   },],
+                   }
+                   ],
                 },
             },
 
@@ -64,10 +66,12 @@ export class AutoScalingStack extends AwsStackBase {
                    metricName: "MemoryReservation",
                    namespace: "AWS/ECS",
                    statistic: "Average", 
-                   metricDimension: [{
+                   metricDimension: [
+                   {
                     name: "ClusterName",
                     value: props.ecsClusterName,
-                   },],
+                   }
+                   ],
                 },
             },
 
