c6a5db519744ad16b486cfa46ee46ec5e449f2f8
joj <young2k15@tutanota.com>
2024-10-09 19:26:48 -0400

    minor fix

--- lib/stacks/launchtemplate-stack.ts
+++ lib/stacks/launchtemplate-stack.ts
@@ -29,7 +29,7 @@ export class LaunchTemplateStack extends AwsStackBase {
             },
             vpcSecurityGroupIds: props.securityGroupIds,
             updateDefaultVersion: true,
-            userData: readFileSync(`${props.userData}`,'utf8')
+            userData: readFileSync(`${props.userData}`,'utf8'),
 
             tags : {
                 Name: `${props.name}-instance`,
