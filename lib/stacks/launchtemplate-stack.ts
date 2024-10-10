fdc3a3a8a305ea0f6f9166bebd5a0db04f7bf343
joj <young2k15@tutanota.com>
2024-10-09 22:37:47 -0400

    launch template test

--- lib/stacks/launchtemplate-stack.ts
+++ lib/stacks/launchtemplate-stack.ts
@@ -1,6 +1,7 @@
 import { Construct } from 'constructs';
 import { AwsStackBase, BaseStackProps } from './stackbase';
 import { LaunchTemplate } from '@cdktf/provider-aws/lib/launch-template'
+import { IamRole } from '@cdktf/provider-aws/lib/iam-role';
 import { readFileSync } from 'fs'
 
 export interface LaunchTemplateConfigs extends BaseStackProps {
@@ -37,5 +38,45 @@ export class LaunchTemplateStack extends AwsStackBase {
             }
 
         })
+
+        this.taskRole = new IamRole(this, `${props.name}-ecs-role`, {
+          name: `${props.name}-ecs-role`,
+          inlinePolicy: [
+            {
+              name: "allow-logs",
+              policy: JSON.stringify({
+                Version: "2012-10-17",
+                Statement: [
+                  {
+                    Effect: "Allow",
+                    Action: ["ecs:*", "ec2:*"],
+                    Resource: "*",
+                  },
+                ],
+              }),
+            },
+          ],
+          assumeRolePolicy: JSON.stringify({
+            Version: "2012-10-17",
+            Statement: [
+              {
+                Action: "sts:AssumeRole",
+                Effect: "Allow",
+                Sid: "",
+                Principal: {
+                  Service: "ecs.amazonaws.com",
+                },
+              },
+              {
+                Action: "sts:AssumeRole",
+                Effect: "Allow",
+                Sid: "",
+                Principal: {
+                  Service: "ec2.amazonaws.com",
+                },
+              },
+            ],
+          }),
+        });
     }
 }
