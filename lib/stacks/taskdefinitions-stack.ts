8ec4a399ab26a405c0c5f4f516da09adc72c26b2
joj <young2k15@tutanota.com>
2024-10-09 23:51:15 -0400

    minor fix

--- lib/stacks/taskdefinitions-stack.ts
+++ lib/stacks/taskdefinitions-stack.ts
@@ -94,9 +94,9 @@ export class taskDefinitionStack extends AwsStackBase {
         this.td = new EcsTaskDefinition(this, `${props.name}-task-definition`, {
             family: `${props.name}-client`,
             memory: "512",
-            cpu: "1024",
+            cpu: "512",
             networkMode: "awsvpc",
-            requiresCompatibilities: ["FARGATE"],
+            requiresCompatibilities: ["EC2"],
             executionRoleArn: executionRole.arn,
             taskRoleArn: taskRole.arn,
 
