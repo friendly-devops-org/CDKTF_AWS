6ef9f62efcbf3578c651d8f55f564f01a41270df
joj <young2k15@tutanota.com>
2024-10-09 23:07:07 -0400

    added instance profile

--- lib/stacks/launchtemplate-stack.ts
+++ lib/stacks/launchtemplate-stack.ts
@@ -1,7 +1,6 @@
 import { Construct } from 'constructs';
 import { AwsStackBase, BaseStackProps } from './stackbase';
 import { LaunchTemplate } from '@cdktf/provider-aws/lib/launch-template'
-import { IamRole } from '@cdktf/provider-aws/lib/iam-role';
 import { readFileSync } from 'fs'
 
 export interface LaunchTemplateConfigs extends BaseStackProps {
@@ -10,60 +9,19 @@ export interface LaunchTemplateConfigs extends BaseStackProps {
     region: string,
     imageId: string,
     instanceType: string,
-//    iamInstanceProfile: string,
+    iamInstanceProfile: string,
     securityGroupIds: string[],
     userData: string,
 }
 
 export class LaunchTemplateStack extends AwsStackBase {
     public launchTemplate: LaunchTemplate;
-    private ecsRole: IamRole;
     constructor(scope: Construct, id: string, props: LaunchTemplateConfigs) {
         super(scope,`${props.name}-${id}` , {
             name: props.name,
             project: props.project,
             region: props.region
         })
-
-        this.ecsRole = new IamRole(this, `${props.name}-ecs-role`, {
-          name: `${props.name}-ecs-role`,
-          inlinePolicy: [
-            {
-              name: "deploy-ecs",
-              policy: JSON.stringify({
-                Version: "2012-10-17",
-                Statement: [
-                  {
-                    Effect: "Allow",
-                    Action: ["ecs:*", "ec2:*"],
-                    Resource: "*",
-                  },
-                ],
-              }),
-            },
-          ],
-          assumeRolePolicy: JSON.stringify({
-            Version: "2012-10-17",
-            Statement: [
-              {
-                Action: "sts:AssumeRole",
-                Effect: "Allow",
-                Sid: "",
-                Principal: {
-                  Service: "ecs.amazonaws.com",
-                },
-              },
-              {
-                Action: "sts:AssumeRole",
-                Effect: "Allow",
-                Sid: "",
-                Principal: {
-                  Service: "ec2.amazonaws.com",
-                },
-              },
-            ],
-          }),
-        });
 
         this.launchTemplate = new LaunchTemplate(this,`${props.name}-launch-template`, {
             instanceType: props.instanceType,
