78ee81b69d54969237e0f44e828692a7475cb4a7
joj <young2k15@tutanota.com>
2024-10-12 21:48:20 -0400

    added route 53

--- main.ts
+++ main.ts
@@ -10,7 +10,7 @@ import { LaunchTemplateStack, LaunchTemplateConfigs } from './lib/stacks/launcht
 import { AutoScalingStack, AutoScalingConfigs } from './lib/stacks/autoscaling-stack';
 import { AppAutoScalingStack, AppAutoScalingConfigs } from './lib/stacks/application-as-stack';
 import { sgStack } from './lib/stacks/securitygroup-stack';
-import { Route53Stack } from './lib/stacks/route53-stack';
+import { Route53Stack, RouteConfigs } from './lib/stacks/route53-stack';
 
 const StackProps: BaseStackProps = {
     name: "first-complete",
