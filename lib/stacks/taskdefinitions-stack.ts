af32c41a676afd1cfdb77e5ff78ab09708967b5c
joj <young2k15@tutanota.com>
2024-10-13 09:53:11 -0400

    increase cpu to minimum

--- lib/stacks/taskdefinitions-stack.ts
+++ lib/stacks/taskdefinitions-stack.ts
@@ -93,8 +93,8 @@ export class taskDefinitionStack extends AwsStackBase {
 
         this.td = new EcsTaskDefinition(this, `${props.name}-task-definition`, {
             family: `${props.name}-client`,
-            memory: "1024",
-            cpu: "512",
+            memory: "2048",
+            cpu: "1024",
             networkMode: "awsvpc",
             requiresCompatibilities: ["FARGATE"],
             executionRoleArn: executionRole.arn,
