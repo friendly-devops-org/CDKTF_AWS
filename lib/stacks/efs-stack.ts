8cd505777f2420e183055c8d82c5da84d175674c
joj <young2k15@tutanota.com>
2024-12-12 21:38:12 -0500

    update

--- lib/stacks/efs-stack.ts
+++ lib/stacks/efs-stack.ts
@@ -1,5 +1,5 @@
 import { Construct } from 'constructs';
-import { AwsStackBase, BaseStackProps } from './stackbase';
+import { BaseStackProps } from './stackbase';
 import { EfsFileSystem } from '@cdktf/provider-aws/lib/efs-file-system'
 import { EfsAccessPoint } from '@cdktf/provider-aws/lib/efs-access-point'
 import { KmsKey } from '@cdktf/provider-aws/lib/kms-key'
@@ -19,14 +19,13 @@ export class efsStack extends EfsFileSystem {
             name: `${props.name}`,
             project: `${props.project}`,
             region: `${props.region}`,
-            subnetId: `${props.subnetId}`,
             securityGroup: `${props.securityGroup}`,
         })
         const kmsKey = new KmsKey(this, `${props.name}-kms-key`, {
             description: "Encryption key for the efs filesystem",
             id: `${props.name}-efs-kms-key`
         })
-        const efs = new EfsFileSystem(this, `${props.name}-efs`, {
+        this.efs = new EfsFileSystem(this, `${props.name}-efs`, {
             creationToken: `${props.name}-${props.project}-efs`,
             encrypted: true, 
             kmsKeyId: kmsKey.id,
