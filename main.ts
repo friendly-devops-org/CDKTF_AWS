5aa69378b18237e3645d3e56785e11e42f91302a
joj <young2k15@tutanota.com>
2024-12-22 04:56:43 -0500

    removed invalid name

--- main.ts
+++ main.ts
@@ -28,7 +28,7 @@ const cluster = new EcsClusterStack(app, "ecs-cluster-stack", StackProps);
 const sGroup = new sgStack(app, "sg-stack", StackProps);
 const db = new dbStack(app, "db-stack", StackProps);
 
-const clusterName = `${cluster.cluster.name}`;
+const clusterName = `${StackProps.name}-${StackProps.project}-cluster`;
 aFile(clusterName);
 
 const DbConfig: DbConfigs = {
